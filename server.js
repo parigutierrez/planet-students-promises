6fc7d782b1baed4e3333b6316db91e4af1bfcb11
parigutierrez <parivash.gutierrez@gmail.com>
2017-07-12 16:17:06 -0500

    Subiendo práctica de promesas

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use('/data', express.static(__dirname+'/data'));
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname+'/promesas.html');
+});
+app.listen(1234);
